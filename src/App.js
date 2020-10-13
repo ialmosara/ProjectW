88696066347170e53288e4382ed0ddbc007a00e3
Manny <ialmosara@gmail.com>
2020-10-13 01:50:22 -0400

    Created nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Navbar from "./components/navbar/Navbar";
 
-function App() {
+import GlobalStyle from './css/Global';
+
+const App = () => {
+  const [navbar, setnavbar] = useState();
+
+  const handleNavbar = () => {
+    setnavbar(!navbar);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar
+        navbarState={navbar}
+        handleNavbar={handleNavbar}
+      />
+      <GlobalStyle />
+    </>
+  )
 }
 
 export default App;
